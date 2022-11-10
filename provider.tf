a3c5b3fb697fb4bf36ef0d1a921ff957e67ef2f2
brooks <brooks.houck@gmail.com>
2022-11-10 20:42:31 +0000

    changing tags again

--- provider.tf
+++ provider.tf
@@ -22,10 +22,10 @@ provider "aws" {
   region = "us-east-1"
     default_tags {
     tags = {
-      environment = "development"
+      Environment = "development"
       platform    = "terraform"
       repo-name   = "scaling-succotash"
-      User        = "Matthew Davis"
+      User        = "MatthewDavis"
     }
   }
 }
