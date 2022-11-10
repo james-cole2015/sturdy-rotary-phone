72c51b3448dce607bf1f934d1e8925c3a82f28d2
brooks <brooks.houck@gmail.com>
2022-11-10 20:24:52 +0000

    minor change to name

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 resource "aws_vpc" "requesting_vpc" {
-    cidr_block = "10.100.0.0/16"
+  cidr_block = "10.100.0.0/16"
 }
 
 resource "aws_vpc" "accepting_vpc" {
-    cidr_block = "10.200.0.0/16"
+  cidr_block = "10.200.0.0/16"
 }
 
 resource "aws_subnet" "requesting-subnet-00" {
-    vpc_id = "aws_vpc.requesting_vpc.id"
-    cidr_block = "10.100.0.0/16"
-    
+  vpc_id     = "aws_vpc.requesting_vpc.id"
+  cidr_block = "10.100.0.0/16"
+
 }
 
-resource "aws_subnet" "accepting-subnet-00" {
-    vpc_id = "aws_vpc.accepting_vpc.id"
-    cidr_block = "10.200.0.0/16"
-    
+resource "aws_subnet" "accepting-subnet-01" {
+  vpc_id     = "aws_vpc.accepting_vpc.id"
+  cidr_block = "10.200.0.0/16"
+
 }
