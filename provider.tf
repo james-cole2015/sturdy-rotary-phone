a7c6ada7d366a152a8aad1649e73a602470da3d5
brooks <brooks.houck@gmail.com>
2022-11-10 20:17:50 +0000

    adding provider config

---
+++ provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "friends_of_fate_903"
+
+    workspaces {
+      name = "sturdy-rotary-phone"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
+
+
+provider "aws" {
+  region = "us-east-1"
+    default_tags {
+    tags = {
+      environment = "production"
+      platform    = "terraform"
+      repo-name   = "scaling-succotash"
+    }
+  }
+}
